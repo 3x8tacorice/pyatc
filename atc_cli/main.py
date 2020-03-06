0a3aa5de1b692f50fd1ebf42657f632ede9ded13
3x8tacorice <t.nkawa.83@gmail.com>
2020-03-07 04:45:24 +0900

    add error handling

--- atc_cli/main.py
+++ atc_cli/main.py
@@ -12,6 +12,7 @@ class Contest(object):
     """A simple contest class."""
 
     def new(self, contest_id, difficulty):
+
         problem = {
             "contest": contest_id,
             "difficulty": difficulty
@@ -22,13 +23,29 @@ class Contest(object):
         if not contest_dir.exists():
             contest_dir.mkdir(parents=True, exist_ok=True)
         
-        # make empty script by difficulty
+        # populate script template by difficulty
         problem_file = contest_dir / Path(difficulty + ".py")
         if not problem_file.exists():
-            # write template script
-            template = pkg_resources.read_text(templates, "solve_problem.py.template")
+            # read script template from package resources
+            SOLVE_PROBLEM_TEMPLATE = "solve_problem.py.template"
+            try:
+                template = pkg_resources.read_text(templates, SOLVE_PROBLEM_TEMPLATE)
+            except Exception as e:
+                print("{}: error with reading template file in package resouces.".format(type(e)))
+                print("clean up temporaly files...")
+                contest_dir.rmdir()
+                return "has cleaned up"
+
+            # write script template by problem info
             populated = template.replace("{{ Problem }}", contest_id + " " + difficulty)
-            problem_file.write_text(populated)
+            try:
+                problem_file.write_text(populated)
+            except Exception as e:
+                print("{}: error with writing script template".format(type(e)))
+                print("clean up temporaly files...")
+                contest_dir.rmdir()
+                return "has cleaned up"
+
         else:
             return "{} already exists.".format(problem_file.name)
 
