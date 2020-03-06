172f4e9c4b52dee09d748669773e2194c4cd1068
3x8tacorice <t.nkawa.83@gmail.com>
2020-03-07 04:13:35 +0900

    add problem template

--- atc_cli/main.py
+++ atc_cli/main.py
@@ -1,10 +1,17 @@
 import fire
 from pathlib import Path
 
+try:
+    import importlib.resources as pkg_resources
+except ImportError:
+    # Try backported to PY<37 `importlib_resources`.
+    import importlib_resources as pkg_resources
+from . import templates
+
 class Contest(object):
     """A simple contest class."""
 
-    def start(self, contest_id, difficulty):
+    def new(self, contest_id, difficulty):
         problem = {
             "contest": contest_id,
             "difficulty": difficulty
@@ -13,12 +20,18 @@ class Contest(object):
         # make contest directory
         contest_dir = Path(contest_id)
         if not contest_dir.exists():
-            contest_dir.mkdir()
+            contest_dir.mkdir(parents=True, exist_ok=True)
         
         # make empty script by difficulty
         problem_file = contest_dir / Path(difficulty + ".py")
         if not problem_file.exists():
-            problem_file.touch()
+            # write template script
+            template = pkg_resources.read_text(templates, "solve_problem.py.template")
+            populated = template.replace("{{ Problem }}", contest_id + " " + difficulty)
+            problem_file.write_text(populated)
+        else:
+            return "{} already exists.".format(problem_file.name)
+
         return problem
 
 
@@ -27,4 +40,4 @@ def contest():
 
 
 if __name__ == "__main__":
-    start()
+    contest()
